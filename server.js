0ab3906ae480879a0eb235c448894225af29452a
vsuresh0255 <vsuresh0255@gmail.com>
2026-07-31 14:59:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -356,6 +356,7 @@ const server = http.createServer(async (req, res) => {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(response));
       } catch (err) {
+        console.log('Order placement threw an exception:', err.message, err.stack);
         res.writeHead(400, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({ error: err.message }));
       }
